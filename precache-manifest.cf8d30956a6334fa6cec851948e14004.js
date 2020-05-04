5f9900bca8be3bde88845f8efdea3325b8e82c07
Hoose <hoose.muu@gmail.com>
2020-05-04 18:13:56 -0500

    Updates

---
+++ precache-manifest.cf8d30956a6334fa6cec851948e14004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377a2a0c535d6e5b619fea3a3fa5a5df",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "9413145264ca43fea7a3",
+    "url": "/calculator/static/css/main.7af35ad4.chunk.css"
+  },
+  {
+    "revision": "278500b1d38493ebb732",
+    "url": "/calculator/static/js/2.ac6761aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.ac6761aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9413145264ca43fea7a3",
+    "url": "/calculator/static/js/main.25cf61ee.chunk.js"
+  },
+  {
+    "revision": "d60859d8a9f5f0e7455b",
+    "url": "/calculator/static/js/runtime-main.1fde7ec2.js"
+  }
+]);
